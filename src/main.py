09b0c6da02a78f15bfc73bb28e712c5ffe2cfa8e
eltempez <tempez.el@gmail.com>
2024-09-04 20:44:06 +0200

    main script creation

---
+++ src/main.py
@@ -0,0 +1,13 @@
+"""Docstring d'une ligne décrivant brièvement ce que fait le programme.
+
+Usage:
+======
+    python nom_de_ce_super_script.py argument1 argument2
+
+    argument1: un entier signifiant un truc
+    argument2: une chaîne de caractères décrivant un bidule
+"""
+
+__author__ = "Eliott TEMPEZ"
+__contact__ = "eliott.tempez@etu.u-paris.fr"
+__date__ = "04-09-2024"
